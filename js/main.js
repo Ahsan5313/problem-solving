c432a91e03883c3df33c6a6d780daceff3067fc9
Ahsan <ahsanulhuq17@gmail.com>
2022-07-09 19:46:11 +0600

    problem solving 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 // Start problem solving with JavaScript at here;
-alert('Hello world')
+// alert('Hello world')
+
+// Write a function that takes an array of string and number. And return interger value;
+
+// let myArray = ['S', 'B', 1, 5, 'k', 6, 7, 'bd', 'fun', 1.2];
+
+// function filteredArray(arr){
+
+//     const result = [];
+//    for(let i = 0; i < arr.length; i++){
+
+//       if(typeof arr[i] === 'number'){
+
+//         result.push(parseInt(arr[i]))
+
+//       }
+//    }
+
+//    return result;
+
+// }
+
+// console.log(filteredArray(myArray))
