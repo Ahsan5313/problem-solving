5bb727bed01a07d59664a85f91dbf5fe5176b0d3
Ahsan <ahsanulhuq17@gmail.com>
2022-07-09 21:19:32 +0600

    problem solving 2

--- js/main.js
+++ js/main.js
@@ -22,3 +22,78 @@
 // }
 
 // console.log(filteredArray(myArray))
+
+// let myArray = [1, 2, 4, 7, 5];
+
+// console.log(myArray[myArray.length - 1])
+
+// function newArray(arr){
+
+//     let newArrayOne = [];
+
+//     let fastEle = arr[0];
+//     let lastEle = arr[arr.length - 1];
+
+//     return newArrayOne += [[fastEle], [lastEle]];
+// }
+
+
+// console.log(newArray(myArray));
+// let myArray = [ 5, 7, 8, 9, 4, 10, 1, 2, 5, 0];
+
+// function myFun (arr){
+
+//     let newArray = [];
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         myArray[i] = arr[arr.length - i - 1]
+
+//     }
+
+//     return newArray;
+
+
+// }
+
+// console.log(myFun(myArray))
+
+// let myArray = [ 5, 7, 8, 9, 4, 10, 1, 2, 5, 0];
+
+// function arrReverse(arr){
+
+//   let newArray = [];
+
+//    for(let i = 0; i < arr.length; i++){
+
+//    newArray[i] = arr[arr.length - i - 1];
+
+//    }
+//    return newArray;
+// }
+// console.log(arrReverse(myArray));
+// console.log(myArray);
+
+// let n = 12.30;
+
+// console.log(typeof n)
+
+// let myArray = [1, 'bulbul', 'bd', 2, 7, 'eid', 'mubarak', 12, 40, 12.10];
+
+// function myFun(arr){
+
+//     let newArray = [];
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         if(typeof arr[i] === 'number'){
+
+//             newArray.push(parseInt(arr[i]))
+//         }
+//     }
+
+//     return newArray;
+
+// }
+
+// console.log(myFun(myArray))
